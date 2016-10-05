e7591d0dd032d3b6142cfeba9d0a566cb8fd6350
dmaish <mainadaniel81@gmail.com>
2016-10-05 12:04:08 +0300

    'start of the utilisation of the SharedPreferences utility class'

--- app/src/main/java/com/example/danielmaina/mytodolist/MainActivity.java
+++ app/src/main/java/com/example/danielmaina/mytodolist/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.danielmaina.mytodolist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-  /** //declare the ListView
+  ///declare the ListView
     ListView lvItems;
     //declare an array to store the items
     ArrayList<String> items;
@@ -23,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         //Atach adapter to the ListView and ArrayList
         itemsAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        items.add("this are your tasks");
+        //items.add("this are your tasks");
     }
 
     public void onAddItem(View v){
@@ -32,30 +36,16 @@ public class MainActivity extends AppCompatActivity {
         itemsAdapter.add(itemText);
         etNewItem.setText("");
 
+
+
     }
 
 
 }
-*/
 
-  //adding recycler view,adapter and layout manager.
-    RecyclerView tasks;
-    RecyclerView.Adapter adapter_tasks;
-    RecyclerView.LayoutManager LayoutManager_tasks;
 
-    @Override
-    protected void onCreate( Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        //if the recycler view has a fixed size
-        tasks.setHasFixedSize(true);
-        //using linearLayout manager
-        LayoutManager_tasks = new LinearLayoutManager(this);
-        //connecting recyclerview to LinearLayoutManager
-        tasks.setLayoutManager(LayoutManager_tasks);
-    }
-}
+
 
 
 
---
+++ app/src/main/java/com/example/danielmaina/mytodolist/SharedPreferences.java
@@ -0,0 +1,7 @@
+package com.example.danielmaina.mytodolist;
+
+/**
+ * Created by danielmaina on 10/5/16.
+ */
+public class SharedPreferences {
+}
